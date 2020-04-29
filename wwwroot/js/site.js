2d6ed80ddc47401a0bf0e4212336e6e4322da633
YilianSource <yiliansource@gmail.com>
2020-04-29 23:51:07 +0200

    Added initial styling concepts

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+document.addEventListener('DOMContentLoaded', function() {
+    const posts = document.querySelector('.posts');
+    const gridOptions = document.querySelectorAll(".post-display-options > *");
+    
+    function setGridAspect(aspect) {
+        gridOptions.forEach(option => {
+            option.classList = option.id.endsWith(aspect)
+                ? "active" : "";
+        });
+        posts.classList = "posts posts-aspect-" + aspect;
+        window.sessionStorage.setItem("aspect", aspect);
+    }
+
+    gridOptions.forEach(option => {
+        option.addEventListener("click", e => {
+            setGridAspect(option.id.substring("post-display-option-".length));
+        });
+    });
+
+    let aspect = window.sessionStorage.getItem("aspect") || "middle";
+    setGridAspect(aspect);
+}, false);
